b89056d561f020966385f94cd0daba9e7d55f4a1
Jerome <jerome.emi@onja.org>
2023-06-20 10:37:07 +0300

    Fixes failing test and updated package.json file to add cypress test script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders Tic tac toe main page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Tic tac toe');
   expect(linkElement).toBeInTheDocument();
 });
